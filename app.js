7bbb6ea06a7d002e5b8d1e870fab4ad6aed0a553
Yeshaiah <729960@student.bedford.ac.uk>
2026-03-16 13:48:40 +0000

    Initial images are clickable now.

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ function StartSystem(TargetName, ImageAmmount){
     const contentContainer = document.querySelector('.content-container');
     let itemsLoaded = 3; 
 
+    const initialImages = contentContainer.querySelectorAll('img');
+    
+    initialImages.forEach((img, index) => {
+        const ImageURl = 'Images/' + TargetName + "/img" + (index + 1) + ".png"
+        img.setAttribute('src', ImageURl);
+        img.addEventListener('click', function(){
+            window.open(ImageURl)
+        });
+    });
+
     loadMoreButton.addEventListener('click', function(){
         const itemsToLoad = 3;
 
